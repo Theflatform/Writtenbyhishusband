ffb9a5ef031a3e1facb36cf84a0a713e5f624fc8
Theflatform <juanmaskolmi77@gmail.com>
2026-03-30 18:27:22 -0500

    Update scripts.js.js

--- scripts.js.js
+++ scripts.js.js
@@ -2,3 +2,20 @@ function MostrarCarta() {
     var texto = document.getElementById("texto-carta");
     texto.style.display = "block";
 }
+
+function crearCorazon() {
+    var corazon = document.createElement("div");
+    corazon.classList.add("corazon");
+    corazon.innerHTML = "❤";
+
+    corazon.style.left = "0px";
+    corazon.style.top = Math.random() * 100 + "%";
+
+    document.querySelector(".cartita-bonita").appendChild(corazon);
+
+    setTimeout(function() {
+        corazon.remove();
+    }, 4000);
+}
+
+setInterval(crearCorazon, 300);
